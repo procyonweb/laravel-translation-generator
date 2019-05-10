b5e3502e48ecd02bede4ded7cb86c1dcb1b400e4
Gabor Koszegi <gabor.koszegi@tert.uk>
2019-05-10 09:23:53 +0200

    init

---
+++ src/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Kg4b0r\TranslationGenerator;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+}
