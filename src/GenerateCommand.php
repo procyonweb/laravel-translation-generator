61161eba34d953fc3f123c674a0f0919ea39edc9
Gabor Koszegi <gabor.koszegi@tert.uk>
2019-05-22 17:20:51 +0200

    adding vue, fixed few json issues

--- src/GenerateCommand.php
+++ src/GenerateCommand.php
@@ -8,7 +8,7 @@ class GenerateCommand extends Command
 {
     protected $signature = 'translation:generate';
 
-    protected $description = 'Generate missing translation strings in php files';
+    protected $description = 'Generate missing translation strings in php and Vue files';
 
     public function __construct()
     {
@@ -17,18 +17,19 @@ class GenerateCommand extends Command
 
     public function handle()
     {
-        $translatables = (new SearchService())->getTranslatableStrings('{app/**/*.php,resources/views/**/*.php}');
+        $translatables = (new SearchService())->getTranslatableStrings('{app/**/*.php,resources/views/**/*.php,resources/js/components/**/*.vue}');
 
         $jsonFile = file_get_contents('resources/lang/hu.json');
         $translations = json_decode($jsonFile, true);
 
         foreach ($translatables as $translatable) {
+            $translatable = str_replace('\\','', $translatable);
             if(!array_key_exists($translatable, $translations))
             {
                 $translations[$translatable] = $translatable;
             }
         }
 
-        file_put_contents('resources/lang/hu.json', json_encode($translations, JSON_PRETTY_PRINT));
+        file_put_contents('resources/lang/hu.json', json_encode($translations, JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE+JSON_UNESCAPED_SLASHES));
     }
 }
