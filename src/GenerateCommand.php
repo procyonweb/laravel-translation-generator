f9bf9194790ed2e6753df39e33c44e823d84c9dc
Gabor Koszegi <gabor.koszegi@tert.uk>
2019-06-03 19:41:29 +0200

    search in subfolders

--- src/GenerateCommand.php
+++ src/GenerateCommand.php
@@ -17,7 +17,7 @@ class GenerateCommand extends Command
 
     public function handle()
     {
-        $translatables = (new SearchService())->getTranslatableStrings('{app/**/*.php,resources/views/**/*.php,resources/js/components/**/*.vue}');
+        $translatables = (new SearchService())->getTranslatableStrings('{app/{**/*,*}/*.php,resources/views/{**/*,*}/*.php,resources/js/{**/*,*}/*.vue}');
 
         $jsonFile = file_get_contents('resources/lang/hu.json');
         $translations = json_decode($jsonFile, true);
